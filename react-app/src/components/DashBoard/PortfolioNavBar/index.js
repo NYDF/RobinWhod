f4da6aeddfd62b8b83e1aef32c4a764c266d08fd
dongfang <yusu.us@gmail.com>
2022-12-02 14:05:23 -0500

    css done

--- react-app/src/components/DashBoard/PortfolioNavBar/index.js
+++ react-app/src/components/DashBoard/PortfolioNavBar/index.js
@@ -4,9 +4,12 @@ import logo from '../../../img/logo.png'
 import NavBarAccount from './NavBarAccount';
 import { NavLink } from 'react-router-dom';
 
+import linkedin from '../../../img/linkedin.png'
+import github from '../../../img/github.png'
+
 import './PortfolioNavBar.css'
 
-function PortfolioNavBar({showModal, setShowModal, closetable}) {
+function PortfolioNavBar({ showModal, setShowModal, closetable }) {
 
 
 
@@ -14,25 +17,37 @@ function PortfolioNavBar({showModal, setShowModal, closetable}) {
 
     <div className='Splash-nav-container'>
 
-      <NavLink
-        to={(`/portfolio`)}>
-        <div className='Splash-nav-left'>
+      <div className='Splash-nav-left'>
+        <NavLink
+          to={(`/portfolio`)}>
+
           <img id='logo-P' src={logo} />
+
+        </NavLink>
+
+        <div className='About-link-port-div'>
+          <div className='About-link-port'>Link to Developer'
+            <a href=" https://www.linkedin.com/in/dongfang-song-25261218a/"><img id='linkedin' src={linkedin} /></a>
+            <a href=" https://github.com/NYDF"><img id='github' src={github} /></a>
+          </div>
         </div>
-      </NavLink>
+
+      </div>
 
       <div className='Portfolio-nav-right' >
+
+
 
         <span className='Portfolio-nav-right-acc-btn' onClick={closetable} >
           <button id="show-acc-btn" > Account </button>
         </span>
 
         <div className="Nav-acc-container">
-        {showModal && (
+          {showModal && (
 
-            <NavBarAccount setShowModal={setShowModal}/>
+            <NavBarAccount setShowModal={setShowModal} />
 
-        )}
+          )}
         </div>
 
       </div>
